26083b5e40b1c9347fa5a88aef55ef3651ed9a61
lamlt <lamlt@smartosc.com>
2021-01-07 15:53:11 +0700

    update game

---
+++ Assets/Scripts/GameGUIManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameGUIManager : Singleton<GameGUIManager>
+{
+    public GameObject homeGui;
+    public GameObject gameGui;
+    public GameObject gameOver;
+
+    public Text scoreCoutingText;
+
+    public override void Awake()
+    {
+        MakeSingleton(false);
+    }
+
+    public void ShowGameGui(bool isShow)
+    {
+        if (gameGui)
+            gameGui.SetActive(isShow);
+
+        if (homeGui)
+            homeGui.SetActive(!isShow);
+    }
+
+    public void ShowGameOver(bool isShow)
+    {
+        if (gameOver)
+            gameOver.SetActive(isShow);
+
+    }
+
+    public void UpdateScoreCounting(int score) // update điểm số 
+    {
+        if (scoreCoutingText)
+            scoreCoutingText.text = "SCORE : x" + score.ToString("00");
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public Dino playerPrefab;
+
+    public Stone[] stonePrefabs;
+
+    public float spawnTime;
+
+    int m_score;
+    bool m_isGameover;
+    bool m_isGamebegun;
+    Dino m_DinoClone;
+
+    public int Score { get => m_score; set => m_score = value; }
+    public bool IsGameover { get => m_isGameover; set => m_isGameover = value; }
+    public bool IsGamebegun { get => m_isGamebegun;}
+
+    public override void Awake()
+    {
+        MakeSingleton(false);
+    }
+
+    public override void Start()
+    {
+        GameGUIManager.Ins.ShowGameGui(false);
+    }
+
+    public void PlayGame()
+    {
+        if (playerPrefab)
+            m_DinoClone = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        StartCoroutine(Spawn());
+        GameGUIManager.Ins.ShowGameGui(true);
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(3f); // 3s đá sẽ rơi
+
+        m_isGamebegun = true;
+        if (stonePrefabs != null && stonePrefabs.Length > 0)
+        {
+            while (!m_isGameover)
+            {
+                int randIdx = Random.Range(0, stonePrefabs.Length);
+
+                if (stonePrefabs[randIdx] != null)
+                {
+                    Instantiate(stonePrefabs[randIdx], new Vector3(m_DinoClone.transform.position.x, Random.Range(6f, 9f), 0f), Quaternion.identity);
+                }
+
+                yield return new WaitForSeconds(spawnTime);
+            }
+        }
+        yield return null;
+    }
+}
